fcb3fade240f7f3a22f4d34e24ebf7d8167067eb
Elizabeth <elizasteprene@gmail.com>
2024-10-09 13:57:45 +0200

    update favicon

--- src/components/About.js
+++ src/components/About.js
@@ -37,7 +37,17 @@ const About = () => {
                     </p>
 
                     <h2> Color grading</h2>
-                    <iframe width="60%" height="20%" src="https://www.youtube.com/embed/yykqSbd-AqE?si=JD1pGv-lGTk5AjUJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <iframe
+                        width="550"
+                        height="300"
+                        src="https://www.youtube.com/embed/yykqSbd-AqE?si=JD1pGv-lGTk5AjUJ"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin"
+                        allowfullscreen>
+
+                    </iframe>
 
                     <h2> maths </h2>
                     <p className='p_left_align'  >
